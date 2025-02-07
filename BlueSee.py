32728192bcc1f82fab7a6f21b4c1271b680eb36b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2025-02-07 14:39:31 -0500

    update

--- BlueSee.py
+++ BlueSee.py
@@ -70,16 +70,18 @@ def BlueSeeScan():
 
 # Function to start BlueSEENetwork
 def scan_network(ip_range):
-    # Scan the network for IP addresses
-    print("\n🔍 Scanning network for connected devices...")
-    arp_request = scapy.ARP(pdst=ip_range)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]
+    if os.geteuid() == 0:
+        # Scan the network for IP addresses
+        print("\n🔍 Scanning network for connected devices...")
+        arp_request = scapy.ARP(pdst=ip_range)
+        broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = broadcast / arp_request
+        answered_list = scapy.srp(arp_request_broadcast, timeout=2, verbose=False)[0]
 
-    for element in answered_list:
-        print(f"🖥️  IP: {element[1].psrc}  |  MAC: {element[1].hwsrc}")
-        print(f"🖥️  IP: {element[1].psrc}  |  MAC: {element[1].hwsrc}", file=BLUE_SEE_SCAN_IP)
-
+        for element in answered_list:
+            print(f"🖥️  IP: {element[1].psrc}  |  MAC: {element[1].hwsrc}")
+            print(f"🖥️  IP: {element[1].psrc}  |  MAC: {element[1].hwsrc}", file=BLUE_SEE_SCAN_IP)
+    else:
+        print("USE ROOT FOR THIS PROGRAM")
 # Allow the user to choose what function to run
 user_choice()
--- DontEdit.py
+++ DontEdit.py
@@ -5,9 +5,9 @@ import scapy.all as scapy  # For network scanning
 import os
 import subprocess
 # Open the output files
-BLUE_SEE_SCAN_ON = open("BLUE_SEE_SCAN_ON.txt", "w")
-BLUE_SEE_SCAN_OFF = open("BLUE_SEE_SCAN_OFF.txt", "w") 
-BLUE_SEE_SCAN_IP = open("BLUE_SEE_SCAN_IP.txt", "w")
+BLUE_SEE_SCAN_ON = open("BLUE_SEE_SCAN_ON.log", "w")
+BLUE_SEE_SCAN_OFF = open("BLUE_SEE_SCAN_OFF.log", "w") 
+BLUE_SEE_SCAN_IP = open("BLUE_SEE_SCAN_IP.log", "w")
 
 # Print the banner
 banner = pyfiglet.figlet_format("BLUE-SEE")
